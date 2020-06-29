df2e4917a3ec810f5dfa7b3d854e37e5cbab367e
Monica Olejniczak <molejniczak@atlassian.com>
2020-06-29 13:03:35 +1000

    Extract package name into a constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+export const PACKAGE_NAME = 'react-loosely-lazy';
+
 export const MODE = {
   RENDER: 'RENDER' as const,
   HYDRATE: 'HYDRATE' as const,
@@ -13,7 +15,7 @@ export const PHASE_LAZY_DELAY = 50;
 
 export const COLLECTED = new Map();
 
-type Settings = {
+export type Settings = {
   CURRENT_MODE: typeof MODE.HYDRATE | typeof MODE.RENDER;
 };
 
