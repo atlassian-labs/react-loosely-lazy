73ce655dc25628966d0e50b4760634ac26947d21
Leo Caseiro <lcaseiro@atlassian.com>
2021-01-07 17:35:18 +1100

    add useCallback and performance.mark for regression investigation

--- src/lazy/placeholders/render.tsx
+++ src/lazy/placeholders/render.tsx
@@ -1,19 +1,52 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useCallback } from 'react';
 
 const usePlaceholderRender = (resolveId: string, content: HTMLElement[]) => {
   const hydrationRef = useRef<HTMLInputElement | null>(null);
   const { current: ssrDomNodes } = useRef(content || ([] as HTMLElement[]));
 
-  useLayoutEffect(() => {
-    const element = hydrationRef.current;
-    const { parentNode } = element || {};
-
+  const insertBeforeParentDom = ({ parentNode, element }: any) => {
     if (parentNode && !parentNode.contains(ssrDomNodes[0])) {
       ssrDomNodes
         .reverse()
         .forEach((node: HTMLElement) =>
           parentNode.insertBefore(node, (element as any).nextSibling)
         );
+    }
+  };
+
+  const insertBeforeParentDomCallback = useCallback(
+    ({ parentNode, element }: any) => {
+      if (parentNode && !parentNode.contains(ssrDomNodes[0])) {
+        ssrDomNodes
+          .reverse()
+          .forEach((node: HTMLElement) =>
+            parentNode.insertBefore(node, (element as any).nextSibling)
+          );
+      }
+    },
+    [ssrDomNodes]
+  );
+
+  useLayoutEffect(() => {
+    const element = hydrationRef.current;
+    const { parentNode } = element || {};
+
+    if (
+      (window as any).performance &&
+      (window as any).performance.mark &&
+      (window as any).isLoadingPhasesForRllMarksEnabled &&
+      (window as any).isLoadingPhasesForRllMarksEnabled()
+    ) {
+      window.performance.mark('jira-spa/rll-component.start');
+    }
+
+    if (
+      (window as any).isLoadingPhasesForRllRefEnabled &&
+      (window as any).isLoadingPhasesForRllRefEnabled()
+    ) {
+      insertBeforeParentDomCallback({ parentNode, element });
+    } else {
+      insertBeforeParentDom({ parentNode, element });
     }
 
     return () => {
